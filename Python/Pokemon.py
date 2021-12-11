3c592b2fd8b902ad4577bf8483020b021cb44549
Vascro <87906464+Vascro@users.noreply.github.com>
2021-12-11 11:58:51 -0600

    cleaned up code. Still trying to figure out random enemy attack each turn and not having enemy attack once enemy or player hits <=0 hp

--- Python/Pokemon.py
+++ Python/Pokemon.py
@@ -1,65 +1,69 @@
 import random
 enemy = "A wild pokemon"
 enemy_health = 100
-enemy_turn = ["fire breath","ice beam","thunderbolt","leaf blade"]
-enemy_move_1 = "fire breath"
-enemy_move_2 = "ice beam"
-enemy_move_3 = "thunderbolt"
-enemy_move_4 = "leaf blade"
+enemy_turn = ["Fire breath","Ice beam","Thunderbolt","Leaf blade"]
+enemy_move = random.choice(enemy_turn)
 player_health = 100
 player_move_1 = "hyperbeam"
 player_move_2 = "dragon breath"
 player_move_3 = "protect"
 player_move_4 = "meteor"
 while True:
-    player_input = input("What do you do? Attack, Run, Cry, check enemy health, check my health ")
-    if player_input == "check enemy health":
+    if enemy_health <= 0:
+        print("Enemy defeated! Gained 30,000 souls!")
+        break
+    if player_health <= 0:
+        print("Dude you suck, git gud nub. GAME OVER")
+        break
+    player_input = input("What do you do? Attack, Run, Cry, Check enemy health, Check my health ")
+    if player_input.casefold() == "Check enemy health".casefold():
         print(enemy_health)
         continue
-    if player_input == "check my health":
+    elif player_input == "Check my health".casefold():
         print(player_health)
         continue
-    if player_input == "run":
+    elif player_input == "Run".casefold():
         print("Got away safely")
         break
-    if player_input == "cry":
+    elif player_input == "Cry".casefold():
         print("Your bloodline is weak and will end with you.")
         input()
-    if player_input == "Attack":
+    elif player_input == "Attack".casefold():
         move_selection = input("Choose an attack: hyperbeam, dragon breath, protect, or meteor. ")
-        if move_selection == "hyperbeam":
+        if move_selection == "Hyperbeam".casefold():
             enemy_health = enemy_health-40
             print("You've fucked him up! He's lost 40 hp. ")
             input()
-        elif move_selection == "dragon breath":
+        elif move_selection == "Dragon breath".casefold():
             enemy_health = enemy_health-75
             print("That was super effective! Dragons are too OP. He's lost 75 hp. ")
             input()
-        elif move_selection == "protect":
+        elif move_selection == "Protect".casefold():
             player_health = player_health+25
             print("You've gained 25 armor")
             input()
-        elif move_selection == "meteor":
-            enemy_health = enemy_health-20
-            print("That wasn't very effective... your enemy can tank meteors bruh. he's only lost 20 hp. ")
+        elif move_selection == "Meteor".casefold():
+            enemy_health = enemy_health-25
+            print("That wasn't very effective... your enemy can tank meteors bruh. he's only lost 25 hp. ")
             input()
         else:
             print("That isn't a move you know, get fuked nerd lmao")
             input()
-        enemy_move = print("Enemy turn, they use "+random.choice(enemy_turn))
-        if enemy_move == "fire breath":
-            player_health = player_health-30
-            print("Enemy used Fire Breath! You take 30 damage")
-        elif enemy_move == "ice beam":
-                player_health = player_health-50
-                print("Enemy used ice beam! You take 50 damage")
-        elif enemy_move == "thunderbolt":
-                player_health = player_health-75
-                print("Enemy used Thunder Bolt! You take 75 damage")
-        elif enemy_move == "leaf blade":
-                player_health = player_health-15
-                print("Enemy used leaf blade! You take 15 damage")
-    else:
+            enemy_move
+    if enemy_move == "Fire breath":
+        player_health = player_health-30
+        print("Enemy used Fire Breath! You take 30 damage")
         continue
-    if enemy_health <= 0 or player_health <= 0:
-        break
+    elif enemy_move == "Ice beam":
+        player_health = player_health-50
+        print("Enemy used Ice beam! You take 50 damage")
+        continue
+    elif enemy_move == "Thunderbolt":
+        player_health = player_health-75
+        print("Enemy used Thunder Bolt! You take 75 damage")
+        continue
+    elif enemy_move == "Leaf blade":
+        player_health = player_health-15
+        print("Enemy used Leaf blade! You take 15 damage")
+        continue
+
