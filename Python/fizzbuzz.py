83538dd8a9844a09fcbcce0805875e24c766743c
Dan Crain <craindaniel94@gmail.com>
2021-12-10 20:10:20 -0600

    fizzbuzz

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -1,1 +1,7 @@
-for i in range(100): print('FizzBuzz'[i*i%3*4:8--i**4%5] or i)
+for i in range(100): 
+    if i%3==0 and i%5==0: print("fizzbuzz")
+    elif i%3==0: print("fizz")
+    elif i%5==0: print("buzz")
+    else: print(i)
+
+
